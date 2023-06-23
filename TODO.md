82aa3e4a99fce93985b8f398971b58ea0f01a4fe
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-06-23 15:57:57 +0300

    new todo

---
+++ TODO.md
@@ -0,0 +1,3 @@
+src - views - app-views - main - clients - list - List
+
+добавляем в крайний првый столбик две кнопки view и delete. Если раскоментить в delete кнопку button, вылетает с ошибкой, разобраться что за ошибка
--- src/views/app-views/main/clients/list/List.js
+++ src/views/app-views/main/clients/list/List.js
@@ -85,7 +85,8 @@ export class List extends Component {
     };
 
     render() {
-        const { newUsers ,users, userProfileVisible, selectedUser } = this.state;
+        const { newUsers, users, userProfileVisible, selectedUser } =
+            this.state;
 
         const tableColumns = [
             {
@@ -149,35 +150,38 @@ export class List extends Component {
             //         compare: (a, b) => a.status.length - b.status.length,
             //     },
             // },
-            // {
-            //     title: "",
-            //     dataIndex: "actions",
-            //     render: (_, elm) => (
-            //         <div className="text-right d-flex justify-content-end">
-            //             <Tooltip title="View">
-            //                 <Button
-            //                     type="primary"
-            //                     className="mr-2"
-            //                     icon={<EyeOutlined />}
-            //                     onClick={() => {
-            //                         this.showUserProfile(elm);
-            //                     }}
-            //                     size="small"
-            //                 />
-            //             </Tooltip>
-            //             <Tooltip title="Delete">
-            //                 <Button
-            //                     danger
-            //                     icon={<DeleteOutlined />}
-            //                     onClick={() => {
-            //                         this.deleteUser(elm.id);
-            //                     }}
-            //                     size="small"
-            //                 />
-            //             </Tooltip>
-            //         </div>
-            //     ),
-            // },
+            {
+                title: "",
+                dataIndex: "actions",
+                render: (_, elm) => {
+                    console.log('elm', elm);
+                    return (
+                        <div className="text-right d-flex justify-content-end">
+                            <Tooltip title="View">
+                                <Button
+                                    type="primary"
+                                    className="mr-2"
+                                    icon={<EyeOutlined />}
+                                    onClick={() => {
+                                        this.showUserProfile(elm);
+                                    }}
+                                    size="small"
+                                />
+                            </Tooltip>
+                            <Tooltip title="Delete">
+                                {/* <Button
+                                danger
+                                icon={<DeleteOutlined />}
+                                onClick={() => {
+                                    this.deleteUser(elm.id);
+                                }}
+                                size="small"
+                            /> */}
+                            </Tooltip>
+                        </div>
+                    );
+                },
+            },
         ];
         return (
             <Card bodyStyle={{ padding: "0px" }}>
